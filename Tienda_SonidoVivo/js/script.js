24b6dbc0810cc28b30336b0368100decb58ceccf
LenavarrDuoc <leo.navarro@duocuc.cl>
2026-09-02 02:50:52 -0400

    Validación de campos formulario de registro y mejora de html de registro (divs mal cerrados).

--- Tienda_SonidoVivo/js/script.js
+++ Tienda_SonidoVivo/js/script.js
@@ -71,3 +71,130 @@ class Header extends HTMLElement {
 }
 customElements.define('main-header', Header);
 
+
+/* ===== FORMULARIO ===== */
+const formulario = document.getElementById("formulario");
+
+if (formulario) {
+    let nombre = document.getElementById("nombre");
+    let run = document.getElementById("run");
+    let correo = document.getElementById("correo");
+    let telefono = document.getElementById("telefono");
+    let fecNac = document.getElementById("fecNac");
+    let btnRegistrarse = document.getElementById("btn-registrarse");
+
+ /*    function validarCamposLlenos() {
+        btnRegistrarse.disabled = !formulario.checkValidity(); // Deshabilita el botón si algún campo requerido está vacío o inválido
+        if (!formulario.checkValidity()) {
+            formulario.reportValidity(); // Muestra las alertas emergentes del navegador en los campos vacíos o inválidos
+        }
+    }
+
+    formulario.addEventListener("input", validarCamposLlenos); // Valida los campos cada vez que el usuario ingresa datos
+    formulario.addEventListener("change", validarCamposLlenos); // Valida los campos cuando el usuario cambia un campo (por ejemplo, selecciona una fecha)
+ */
+    formulario.addEventListener("submit", function(event){
+        //validación nombre:
+        if(!/^[A-Za-zÁÉÍÓÚáéíóúÑñÜü\s]+$/.test(nombre.value)){
+            event.preventDefault();
+            alert("El nombre debe contener letras y espacios");
+            nombre.focus();
+            return
+        }
+        else if (nombre.value.length < 3){
+            event.preventDefault();
+            alert("El nombre debe tener al menos 3 caracteres");
+            nombre.focus();
+            return
+        }
+        
+        //validación run:
+        if(!/^[0-9kK-]+$/.test(run.value)){
+            event.preventDefault();
+            alert("El RUN debe contener solo números, guión y dígito verificador");
+            run.focus();
+            return
+        }
+        else if(run.value.length <9 || run.value.length >10){
+            event.preventDefault();
+            alert("El RUN debe tener entre 9 y 10 caracteres");
+            run.focus();
+            return
+        }
+        
+        //validación run/dv:
+        let dvValido = run.value.slice(-2)[0].toUpperCase() === "-" && (run.value.trim().slice(-1).toUpperCase() === "K" || !isNaN(run.value.trim().slice(-1))); 
+        if(!dvValido){
+            event.preventDefault();
+            alert("El RUN debe terminar en - y un dígito verificador válido (0 a 9 o K)");
+            run.focus();
+            return
+
+        }
+        
+        //validación correo electrónico:
+        let dominiovalido = correo.value.trim().toLowerCase().endsWith(".com") || correo.value.trim().toLowerCase().endsWith(".cl") || correo.value.trim().toLowerCase().endsWith(".org");
+        if(!correo.checkValidity() || !dominiovalido){ /*require_tld verifica quie tenga dominio si se declara "true"*/
+            event.preventDefault();
+            alert("Debe ingresar un correo válido");
+            correo.focus();
+            return
+        }
+
+        //validación teléfono:
+        if(!/^[0-9+]+$/.test(telefono.value)){
+            event.preventDefault();
+            alert("El teléfono debe contener solo números y el signo +");
+            telefono.focus();
+            return
+        }else if(telefono.value.length !== 12 || !telefono.value.startsWith("+")){
+            event.preventDefault();
+            alert("El teléfono debe tener 12 caracteres e iniciar con +");
+            telefono.focus();
+            return
+        }
+
+        //validación fecha de nacimiento:
+        let fecha = new Date(fecNac.value); //fecha completa ingresada
+        let anio = fecha.getFullYear(); //extracción de año de fecha ingresada
+        let fechaActual = new Date() //fecha actual de sistema
+        const difAnios= fechaActual.getFullYear() - fecha.getFullYear();
+        const difMeses = fecha.getMonth() - fechaActual.getMonth();
+        const difDias = fecha.getDate() - fechaActual.getDate();
+        if(anio < 1900 || fecha > fechaActual){ //compara que año ingresado no sea inferior a 1900 y que fecha ingresada comnpleta no sea superior a la actual al momento de ingreso.
+            event.preventDefault();
+            alert("Debe ingresar una fecha de nacimiento inferior a la fecha actual");
+            fecNac.focus();
+            return
+        }
+        else if(difAnios < 18){
+            event.preventDefault();
+            alert("Debe ser mayor de edad para registrarse.");
+            fecNac.focus();
+            return
+        }
+        else if(difAnios === 18){
+            if(difMeses > 0 || (difMeses === 0 && difDias > 0)){
+                event.preventDefault();
+                alert("Debe ser mayor de edad para registrarse.");
+                fecNac.focus();
+                return;
+            }
+        }
+
+        // Verifica si todos los campos requeridos están llenos y válidos
+        if (formulario.checkValidity()) {
+            /* si se realiza un envío de datos para API, debe evitarse el envío nativo HTML
+            colocando un event.preventDefault aquí*/
+            btnRegistrarse.disabled = true; // Deshabilita el botón de registro para evitar envíos múltiples
+            let mensaje = "Registro válido. Enviando datos...";
+            console.log(mensaje);
+            alert(mensaje);
+            // Aquí ejecutas la lógica para registrar al usuario o iniciar sesión
+        } else {
+            // Muestra las alertas emergentes del navegador en los campos vacíos
+            formulario.reportValidity();
+        }
+
+    });
+}
